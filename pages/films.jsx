57f8841f8c41b76b9e15127100533056cf171b5e
nicolascampbell <nicodsm7@gmail.com>
2023-02-05 21:08:34 +0100

    wip

--- pages/films.jsx
+++ pages/films.jsx
@@ -97,50 +97,466 @@ const MeasuredImage = ({ item, onSizeChange }) => {
   )
 }
 const lastFilmLg = [
-  { w: 12, h: 2, x: 2, y: 0, i: '0', moved: false, static: false },
-  { w: 6, h: 2, x: 4, y: 2, i: '1', moved: false, static: false },
-  { w: 6, h: 2, x: 4, y: 4, i: '2', moved: false, static: false },
-  { w: 6, h: 2, x: 4, y: 6, i: '3', moved: false, static: false },
-  { w: 6, h: 2, x: 0, y: 8, i: '4', moved: false, static: false },
-  { w: 6, h: 2, x: 2, y: 10, i: '5', moved: false, static: false },
-  { w: 6, h: 2, x: 4, y: 12, i: '6', moved: false, static: false },
-  { w: 6, h: 2, x: 2, y: 14, i: '7', moved: false, static: false },
-  { w: 6, h: 2, x: 2, y: 16, i: '8', moved: false, static: false },
-  { w: 6, h: 2, x: 0, y: 18, i: '9', moved: false, static: false },
-  { w: 6, h: 2, x: 4, y: 20, i: '10', moved: false, static: false },
-  { w: 6, h: 2, x: 4, y: 22, i: '11', moved: false, static: false },
-  { w: 6, h: 2, x: 2, y: 24, i: '12', moved: false, static: false },
-  { w: 6, h: 2, x: 4, y: 26, i: '13', moved: false, static: false },
-  { w: 6, h: 2, x: 4, y: 28, i: '14', moved: false, static: false },
-  { w: 6, h: 2, x: 4, y: 30, i: '15', moved: false, static: false },
-  { w: 6, h: 2, x: 4, y: 32, i: '16', moved: false, static: false },
-  { w: 6, h: 2, x: 0, y: 34, i: '17', moved: false, static: false },
-  { w: 6, h: 2, x: 2, y: 36, i: '18', moved: false, static: false },
-  { w: 6, h: 2, x: 2, y: 38, i: '19', moved: false, static: false },
-  { w: 6, h: 2, x: 4, y: 40, i: '20', moved: false, static: false },
-  { w: 6, h: 2, x: 4, y: 42, i: '21', moved: false, static: false },
-  { w: 6, h: 2, x: 2, y: 44, i: '22', moved: false, static: false },
-  { w: 6, h: 2, x: 4, y: 46, i: '23', moved: false, static: false },
-  { w: 6, h: 2, x: 4, y: 48, i: '24', moved: false, static: false },
-  { w: 6, h: 2, x: 4, y: 50, i: '25', moved: false, static: false },
-  { w: 6, h: 2, x: 4, y: 52, i: '26', moved: false, static: false },
-  { w: 6, h: 2, x: 4, y: 54, i: '27', moved: false, static: false },
-  { w: 6, h: 2, x: 0, y: 56, i: '28', moved: false, static: false },
-  { w: 6, h: 2, x: 4, y: 58, i: '29', moved: false, static: false },
-  { w: 6, h: 2, x: 2, y: 60, i: '30', moved: false, static: false },
-  { w: 6, h: 2, x: 0, y: 62, i: '31', moved: false, static: false },
-  { w: 6, h: 2, x: 0, y: 64, i: '32', moved: false, static: false },
-  { w: 6, h: 2, x: 4, y: 66, i: '33', moved: false, static: false },
-  { w: 6, h: 2, x: 0, y: 68, i: '34', moved: false, static: false }
-]
-const BREAKPOINTS = { md: 600, xs: 480, xxs: 0 }
+  {
+    w: 5,
+    h: 5.041666666666667,
+    x: 0,
+    y: 0,
+    i: '0',
+    minH: 5.041666666666667,
+    maxH: 5.041666666666667,
+
+    static: false,
+    isDraggable: true
+  },
+  {
+    w: 2,
+    h: 2.188055419921875,
+    x: 5,
+    y: 0,
+    i: '1',
+    minH: 2.188055419921875,
+    maxH: 2.188055419921875,
+
+    static: false,
+    isDraggable: true
+  },
+  {
+    w: 7,
+    h: 6.951388549804688,
+    x: 5,
+    y: 2.188055419921875,
+    i: '2',
+    minH: 6.951388549804688,
+    maxH: 6.951388549804688,
+
+    static: false,
+    isDraggable: true
+  },
+  {
+    w: 3,
+    h: 3.142777760823568,
+    x: 2,
+    y: 5.041666666666667,
+    i: '3',
+    minH: 3.142777760823568,
+    maxH: 3.142777760823568,
+
+    static: false,
+    isDraggable: true
+  },
+  {
+    w: 6,
+    h: 5.996666463216146,
+    x: 0,
+    y: 9.139443969726562,
+    i: '4',
+    minH: 5.996666463216146,
+    maxH: 5.996666463216146,
+
+    static: false,
+    isDraggable: true
+  },
+  {
+    w: 2,
+    h: 3.881944274902344,
+    x: 6,
+    y: 9.139443969726562,
+    i: '5',
+    minH: 3.881944274902344,
+    maxH: 3.881944274902344,
+
+    static: false,
+    isDraggable: true
+  },
+  {
+    w: 2,
+    h: 2.188055419921875,
+    x: 8,
+    y: 9.139443969726562,
+    i: '6',
+    minH: 2.188055419921875,
+    maxH: 2.188055419921875,
+
+    static: false,
+    isDraggable: true
+  },
+  {
+    w: 4,
+    h: 4.086666615804036,
+    x: 8,
+    y: 11.327499389648438,
+    i: '7',
+    minH: 4.086666615804036,
+    maxH: 4.086666615804036,
+
+    static: false,
+    isDraggable: true
+  },
+  {
+    w: 7,
+    h: 6.951388549804688,
+    x: 5,
+    y: 15.414166005452474,
+    i: '8',
+    minH: 6.951388549804688,
+    maxH: 6.951388549804688,
+
+    static: false,
+    isDraggable: true
+  },
+  {
+    w: 5,
+    h: 5.041666666666667,
+    x: 0,
+    y: 15.136110432942708,
+    i: '9',
+    minH: 5.041666666666667,
+    maxH: 5.041666666666667,
+
+    static: false,
+    isDraggable: true
+  },
+  {
+    w: 2,
+    h: 3.881944274902344,
+    x: 5,
+    y: 22.365554555257162,
+    i: '10',
+    minH: 3.881944274902344,
+    maxH: 3.881944274902344,
+
+    static: false,
+    isDraggable: true
+  },
+  {
+    w: 4,
+    h: 4.086666615804036,
+    x: 1,
+    y: 20.177777099609376,
+    i: '11',
+    minH: 4.086666615804036,
+    maxH: 4.086666615804036,
+
+    static: false,
+    isDraggable: true
+  },
+  {
+    w: 5,
+    h: 10.303611246744792,
+    x: 7,
+    y: 22.365554555257162,
+    i: '12',
+    minH: 10.303611246744792,
+    maxH: 10.303611246744792,
+
+    static: false,
+    isDraggable: true
+  },
+  {
+    w: 5,
+    h: 5.041666666666667,
+    x: 0,
+    y: 31.289165496826175,
+    i: '13',
+    minH: 5.041666666666667,
+    maxH: 5.041666666666667,
+
+    static: false,
+    isDraggable: true
+  },
+  {
+    w: 5,
+    h: 5.041666666666667,
+    x: 2,
+    y: 26.247498830159508,
+    i: '14',
+    minH: 5.041666666666667,
+    maxH: 5.041666666666667,
+
+    static: false,
+    isDraggable: true
+  },
+  {
+    w: 10,
+    h: 9.80499979654948,
+    x: 1,
+    y: 36.33083216349284,
+    i: '15',
+    minH: 9.80499979654948,
+    maxH: 9.80499979654948,
+
+    static: false,
+    isDraggable: true
+  },
+  {
+    w: 4,
+    h: 4.086666615804036,
+    x: 8,
+    y: 114.18916549682616,
+    i: '16',
+    minH: 4.086666615804036,
+    maxH: 4.086666615804036,
+
+    static: false,
+    isDraggable: true
+  },
+  {
+    w: 4,
+    h: 4.086666615804036,
+    x: 8,
+    y: 46.135831960042324,
+    i: '17',
+    minH: 4.086666615804036,
+    maxH: 4.086666615804036,
+
+    static: false,
+    isDraggable: true
+  },
+  {
+    w: 2,
+    h: 3.881944274902344,
+    x: 6,
+    y: 46.135831960042324,
+    i: '18',
+    minH: 3.881944274902344,
+    maxH: 3.881944274902344,
+
+    static: false,
+    isDraggable: true
+  },
+  {
+    w: 12,
+    h: 11.715000406901042,
+    x: 0,
+    y: 50.22249857584636,
+    i: '19',
+    minH: 11.715000406901042,
+    maxH: 11.715000406901042,
+
+    static: false,
+    isDraggable: true
+  },
+  {
+    w: 5,
+    h: 5.041666666666667,
+    x: 7,
+    y: 61.9374989827474,
+    i: '20',
+    minH: 5.041666666666667,
+    maxH: 5.041666666666667,
+
+    static: false,
+    isDraggable: true
+  },
+  {
+    w: 6,
+    h: 5.996666463216146,
+    x: 6,
+    y: 66.97916564941407,
+    i: '21',
+    minH: 5.996666463216146,
+    maxH: 5.996666463216146,
+
+    static: false,
+    isDraggable: true
+  },
+  {
+    w: 3,
+    h: 6.030833435058594,
+    x: 2,
+    y: 101.24111048380533,
+    i: '22',
+    minH: 6.030833435058594,
+    maxH: 6.030833435058594,
+
+    static: false,
+    isDraggable: true
+  },
+  {
+    w: 6,
+    h: 5.996666463216146,
+    x: 0,
+    y: 61.9374989827474,
+    i: '23',
+    minH: 5.996666463216146,
+    maxH: 5.996666463216146,
+
+    static: false,
+    isDraggable: true
+  },
+  {
+    w: 3,
+    h: 3.142777760823568,
+    x: 3,
+    y: 67.93416544596354,
+    i: '24',
+    minH: 3.142777760823568,
+    maxH: 3.142777760823568,
+
+    static: false,
+    isDraggable: true
+  },
+  {
+    w: 8,
+    h: 7.906388854980468,
+    x: 0,
+    y: 114.18916549682616,
+    i: '25',
+    minH: 7.906388854980468,
+    maxH: 7.906388854980468,
+
+    static: false,
+    isDraggable: true
+  },
+  {
+    w: 6,
+    h: 12.452500406901041,
+    x: 6,
+    y: 72.97583211263021,
+    i: '26',
+    minH: 12.452500406901041,
+    maxH: 12.452500406901041,
+
+    static: false,
+    isDraggable: true
+  },
+  {
+    w: 6,
+    h: 5.996666463216146,
+    x: 0,
+    y: 71.0769432067871,
+    i: '27',
+    minH: 5.996666463216146,
+    maxH: 5.996666463216146,
+
+    static: false,
+    isDraggable: true
+  },
+  {
+    w: 6,
+    h: 12.452500406901041,
+    x: 0,
+    y: 77.07360967000325,
+    i: '28',
+    minH: 12.452500406901041,
+    maxH: 12.452500406901041,
+
+    static: false,
+    isDraggable: true
+  },
+  {
+    w: 2,
+    h: 2.188055419921875,
+    x: 4,
+    y: 46.135831960042324,
+    i: '29',
+    minH: 2.188055419921875,
+    maxH: 2.188055419921875,
+
+    static: false,
+    isDraggable: true
+  },
+  {
+    w: 12,
+    h: 11.715000406901042,
+    x: 0,
+    y: 89.52611007690429,
+    i: '30',
+    minH: 11.715000406901042,
+    maxH: 11.715000406901042,
+
+    static: false,
+    isDraggable: true
+  },
+  {
+    w: 7,
+    h: 6.951388549804688,
+    x: 5,
+    y: 101.24111048380533,
+    i: '31',
+    minH: 6.951388549804688,
+    maxH: 6.951388549804688,
+
+    static: false,
+    isDraggable: true
+  },
+  {
+    w: 6,
+    h: 5.996666463216146,
+    x: 0,
+    y: 108.19249903361002,
+    i: '32',
+    minH: 5.996666463216146,
+    maxH: 5.996666463216146,
+
+    static: false,
+    isDraggable: true
+  },
+  {
+    w: 6,
+    h: 5.996666463216146,
+    x: 6,
+    y: 108.19249903361002,
+    i: '33',
+    minH: 5.996666463216146,
+    maxH: 5.996666463216146,
+
+    static: false,
+    isDraggable: true
+  },
+  {
+    w: 3,
+    h: 6.030832926432292,
+    x: 0,
+    y: 124.195277150472,
+    i: '34',
+    minH: 6.030832926432292,
+    maxH: 6.030832926432292,
+    moved: false,
+    static: false,
+    isDraggable: true
+  }
+].map((item) => ({ ...item, isDraggable: true }))
+const CustomGridItemComponent = React.forwardRef(
+  (
+    {
+      style,
+      className,
+      onMouseDown,
+      onMouseUp,
+      onTouchEnd,
+      customChild,
+      children,
+
+      ...props
+    },
+    ref
+  ) => {
+    return (
+      <div
+        style={{ ...style }}
+        className={className}
+        ref={ref}
+        onMouseDown={onMouseDown}
+        onMouseUp={onMouseUp}
+        onTouchEnd={onTouchEnd}
+      >
+        {/* Some other content */}
+        {customChild}
+        {/* Make sure to include children to add resizable handle */}
+        {children}
+      </div>
+    )
+  }
+)
+const BREAKPOINTS = { sm: 700, xs: 480, xxs: 0 }
 const ImageGrid = () => {
   const [layouts, setLayouts] = React.useState(
-    generateLayout(BREAKPOINTS, lastFilm.length, { md: lastFilmLg })
+    generateLayout(BREAKPOINTS, lastFilm.length, { sm: lastFilmLg })
   )
-  const [breakpoint, setBreakpoint] = React.useState('md')
+  const [breakpoint, setBreakpoint] = React.useState('sm')
   function handleItemSizeChange(size, index) {
-    console.log(size, index)
     setLayouts(
       produce((draftState) => {
         // From formula pixelHeight = (rowHeight * h) + (marginH * (h - 1))
@@ -163,7 +579,7 @@ const ImageGrid = () => {
       className="layout"
       layouts={layouts}
       breakpoints={BREAKPOINTS}
-      cols={{ md: 12, xs: 4, xxs: 2 }}
+      cols={{ sm: 12, xs: 4, xxs: 2 }}
       rowHeight={10}
       margin={[50, 50]}
       onLayoutChange={handleLayoutChange}
@@ -171,12 +587,16 @@ const ImageGrid = () => {
     >
       {/* lastFilm is an array of objects with src and other stuff about the images*/}
       {lastFilm.map((item, index) => (
-        <div key={index} className={'box'}>
-          <MeasuredImage
-            item={item}
-            onSizeChange={(size) => handleItemSizeChange(size, index)}
-          />
-        </div>
+        <CustomGridItemComponent
+          key={index}
+          className={'box'}
+          customChild={
+            <MeasuredImage
+              item={item}
+              onSizeChange={(size) => handleItemSizeChange(size, index)}
+            />
+          }
+        />
       ))}
     </ResponsiveGridLayout>
   )
