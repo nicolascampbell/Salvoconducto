92930b34691f5f81786b997b8ae6d7d3fb6f3218
nicolascampbell <nicodsm7@gmail.com>
2023-04-18 15:36:12 +0200

    added blur and sort images

--- components/ImageList.jsx
+++ components/ImageList.jsx
@@ -7,15 +7,20 @@ import useMediaQuery from '@mui/material/useMediaQuery'
 import 'yet-another-react-lightbox/styles.css'
 import Lightbox from 'yet-another-react-lightbox'
 import Zoom from 'yet-another-react-lightbox/plugins/zoom'
+import { LazyLoadImage } from 'react-lazy-load-image-component'
 import { getRandomInt } from 'utils'
+import 'react-lazy-load-image-component/src/effects/blur.css'
+
 import { API_URL } from 'utils/config'
 function purifyApiImages(apiImages) {
-  return apiImages.map((image) => ({
-    src: `${API_URL}${image.attributes.url}`,
-    height: image.attributes.height,
-    width: image.attributes.width,
-    id: image.id
-  }))
+  return apiImages
+    .sort((a, b) => a.attributes.name >= b.attributes.name)
+    .map((image) => ({
+      src: `${API_URL}${image.attributes.url}`,
+      height: image.attributes.height,
+      width: image.attributes.width,
+      id: image.id
+    }))
 }
 function getPhotoWidth(relevance) {
   if (relevance === 0) {
@@ -31,31 +36,29 @@ function getPhotoWidth(relevance) {
 export default function MasonryImageList({ images, colsAmount = 2 }) {
   const [open, setOpen] = React.useState(null)
   const router = useRouter()
+  const matchesSmallDevices = useMediaQuery('(max-width:768px)')
 
   const pureImages = React.useMemo(() => {
     return purifyApiImages(images)
   }, [images])
   return (
     <Box className="d-flex justify-content-center">
-      <ImageList variant="masonry" gap={10} cols={2}>
+      <ImageList variant="masonry" gap={30} cols={matchesSmallDevices ? 1 : 2}>
         {pureImages.map((image, index) => {
           return (
             <ImageListItem
               key={image.id}
               sx={{ display: 'flex', justifyContent: 'end' }}
             >
-              <img
+              <LazyLoadImage
                 key={image.id}
                 src={image.src}
-                height={image.height}
                 width={image.width}
                 alt={'Picture of last film'}
                 loading="lazy"
                 className="preview-imgs"
                 onClick={() => setOpen(index)}
-                style={{
-                  width: getPhotoWidth(getRandomInt(2, 8))
-                }}
+                effect="blur"
               />
             </ImageListItem>
           )
