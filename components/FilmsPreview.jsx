c84ad5b243ac4b61bdc642529e58e6e6c0173c7c
nicolascampbell <nicodsm7@gmail.com>
2024-02-25 19:03:16 +0100

    fix: deleted bootstrap react

--- components/FilmsPreview.jsx
+++ components/FilmsPreview.jsx
@@ -2,7 +2,6 @@ import * as React from 'react'
 import Box from '@mui/material/Box'
 import { flattenFilms } from 'utils'
 import CustomImage from '@/components/CustomImage'
-import Carousel from 'react-bootstrap/Carousel'
 export default function FilmsPreview({ films }) {
   const flatFilms = React.useMemo(() => {
     return flattenFilms(films)
@@ -10,26 +9,6 @@ export default function FilmsPreview({ films }) {
   const maxHeight = Math.min(...flatFilms.map(film => film.height))
   return (
     <Box className="d-flex justify-content-center definition no-padd">
-      <Carousel
-        indicators={false}
-        controls={false}
-        interval={50}
-        fade
-        style={{ overflow: 'hidden' }}
-      >
-        {flatFilms.map((film, index) => {
-          return (
-            <Carousel.Item key={film.imageId}>
-              <CustomImage
-                key={film.imageId}
-                src={film.src}
-                alt={`Preview of film ${ film.key }}`} 
-                height={maxHeight}
-              />
-            </Carousel.Item>
-          )
-        })}
-      </Carousel>
     </Box>
   )
 }
